39e878b64c4cfa825381cd2f29aec1e202384244
Polina Sokolova <polina2001sokolovas@gmail.com>
2023-08-01 20:34:13 +0200

    finished foru challenges

--- 04-Front-End/04-JS-and-DOM/04-Fake-Inbox/lib/inbox.js
+++ 04-Front-End/04-JS-and-DOM/04-Fake-Inbox/lib/inbox.js
@@ -6,19 +6,41 @@ import runChallenges from "../spec/inbox_examiner.js";
 
 const hasNewMessage = () => {
   // TODO: return true with a probability of 20%.
+  return Math.random() < 0.2;
 };
-
+const sample = array => array[Math.floor(Math.random() * array.length)];
 const newMessage = () => {
+  const senders = ['GitHub', 'University', 'UDEMY', 'Shazam'];
+  const subjects = ['You Got Mail!', 'Welcome!', 'Congraulations!', 'Did you miss us?'];
+  return {
+    sender: sample(senders),
+    subject: sample(subjects)
+  };
   // TODO: return a random message as an object with two keys, subject and sender
 };
 
 const appendMessageToDom = (message) => {
+  const line = `<div class="row message unread">
+          <div class="col-3">${message.sender}</div>
+          <div class="col-9">${message.subject} :)</div>
+        </div>`;
+  document.getElementById("inbox").insertAdjacentHTML('afterbegin', line);
   // TODO: append the given message to the DOM (as a new row of `#inbox`)
+};
+
+const updateUnreadCount = () => {
+  const unreadCount = document.querySelectorAll('.message.unread').length;
+  document.getElementById('count').innerText = `(${unreadCount})`;
+  document.title = `(${unreadCount}) Fake Inbox`;
 };
 
 const refresh = () => {
   // TODO: Implement the global refresh logic. If there is a new message,
   //       append it to the DOM. Update the unread counter in title as well.
+  if (hasNewMessage()) {
+    appendMessageToDom(newMessage());
+    updateUnreadCount();
+  }
 };
 
 // Do not remove these lines.
