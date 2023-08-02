af5ea7206edd3cf821e297c334b3361d0304c10c
Polina Sokolova <polina2001sokolovas@gmail.com>
2023-08-02 14:01:49 +0200

    r

--- 04-Front-End/05-Events-and-Iterators/02-My-First-Event-Listener/lib/listener.js
+++ 04-Front-End/05-Events-and-Iterators/02-My-First-Event-Listener/lib/listener.js
@@ -1,1 +1,8 @@
 // TODO: React to a click on the button!
+const btn = document.getElementById("clickme");
+const audio = new Audio('sound.mp3');
+btn.addEventListener("click", () => {
+  btn.innerText = "Bingo!";
+  btn.classList.add("disabled");
+  audio.play();
+});
--- 04-Front-End/05-Events-and-Iterators/03-Lottery-Scratchcards/lib/scratchcards.js
+++ 04-Front-End/05-Events-and-Iterators/03-Lottery-Scratchcards/lib/scratchcards.js
@@ -1,1 +1,31 @@
 // TODO: Write your gambling application here.
+const stratchcards = document.querySelectorAll(".scratchcard");
+const balance = document.getElementById("balance");
+let score = 100;
+stratchcards.forEach((card) => {
+  card.addEventListener("click", () => {
+    if (score > 0 && card.dataset.scratched !== "true") {
+      card.innerText = card.dataset.amount;
+      card.dataset.scratched = true;
+      const cardValue = parseInt(card.innerText, 10);
+      score = score - 10 + cardValue;
+      balance.innerText = score;
+    };
+  });
+});
+
+// const balance = document.querySelector("#balance");
+// const scratchcards = document.querySelectorAll(".scratchcard");
+// const price = 10;
+//
+// scratchcards.forEach((scratchcard) => {
+//   scratchcard.addEventListener("click", () => {
+//     const currentBalance = parseInt(balance.innerText, 10);
+//     const scratchcardAmount = parseInt(scratchcard.dataset.amount, 10);
+//     if (currentBalance > 0 && scratchcard.dataset.scratched !== "true") {
+//       scratchcard.dataset.scratched = "true";
+//       balance.innerText = currentBalance - price + scratchcardAmount;
+//       scratchcard.innerText = `${scratchcardAmount}€`;
+//     }
+//   });
+// });
