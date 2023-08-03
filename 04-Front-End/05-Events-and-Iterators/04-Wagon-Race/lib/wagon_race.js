ca64c264b71f905977607cade388d4b389112492
Polina Sokolova <polina2001sokolovas@gmail.com>
2023-08-03 13:49:08 +0200

    finished

--- 04-Front-End/05-Events-and-Iterators/04-Wagon-Race/lib/wagon_race.js
+++ 04-Front-End/05-Events-and-Iterators/04-Wagon-Race/lib/wagon_race.js
@@ -1,1 +1,21 @@
 // TODO: write your code here
+const First = document.querySelectorAll("#player1-race td");
+const Second = document.querySelectorAll("#player2-race td");
+let indexFirst = 0;
+let indexSecond = 0;
+
+const race = document.addEventListener("keyup", (event) => {
+  if ((indexFirst === First.length - 1) || (indexSecond === First.length - 1)) {
+    const w = (indexFirst === First.length - 1) ? alert('playerOne is the winner') : alert('playerTwo is the winner');
+    document.location.reload();
+  }
+  if (event.key === "q") {
+    First[indexFirst].classList.remove('active');
+    First[indexFirst + 1].classList.add('active');
+    indexFirst += 1;
+  } else if (event.key === "p") {
+    Second[indexSecond].classList.remove('active');
+    Second[indexSecond + 1].classList.add('active');
+    indexSecond += 1;
+  }
+});
