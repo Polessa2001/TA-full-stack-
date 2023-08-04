2c29759ec30cf68ac8e7dfc634d9b1363aead87d
Polina Sokolova <polina2001sokolovas@gmail.com>
2023-08-04 15:24:28 +0200

    two challenges finished

--- 04-Front-End/06-HTTP-and-API/01-Click-me/lib/index.js
+++ 04-Front-End/06-HTTP-and-API/01-Click-me/lib/index.js
@@ -1,4 +1,8 @@
 const displayAlertOnButtonClick = () => {
+  const btn = document.querySelector(".btn-success");
+  btn.addEventListener("click", () => {
+    alert("Thank you!");
+  });
   // TODO: Select the big green button
   // TODO: Bind the `click` event to the button
   // TODO: On click, display `Thank you!` in a JavaScript alert!
--- 04-Front-End/06-HTTP-and-API/02-My-Favourite-Sports/lib/index.js
+++ 04-Front-End/06-HTTP-and-API/02-My-Favourite-Sports/lib/index.js
@@ -1,1 +1,17 @@
 // TODO: write your code here!
+const boxes = document.querySelectorAll(".clickable");
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    box.classList.toggle("active");
+  });
+});
+
+// const boxToggleActive = (event) => {
+//   event.currentTarget.classList.toggle("active");
+// };
+//
+// const boxOnClick = (box) => {
+//   box.addEventListener("click", boxToggleActive);
+// };
+//
+// boxes.forEach(boxOnClick);
