f80b3ce72f6e6c384d35e5a710ac74cafbbcfd30
Polina Sokolova <polina2001sokolovas@gmail.com>
2023-08-06 21:47:27 +0200

    four challenges done

--- 04-Front-End/06-HTTP-and-API/04-The-Fork-Restaurants/lib/index.js
+++ 04-Front-End/06-HTTP-and-API/04-The-Fork-Restaurants/lib/index.js
@@ -1,18 +1,33 @@
-// Todo: Use the Fork Api to get a filtered list of restaurants, depending on the category selected (and as a bonus, the location)
-
 const searchForm = document.getElementById("searchForm");
+const resultsList = document.querySelector("#restaurant-list")
+const insertResults = (data) => {
+  resultsList.innerHTML = "";
+  if (data.length > 0) {
+    data.forEach((restaurant) => {
+      resultsList.insertAdjacentHTML("beforeend", `<li>${restaurant.name} in ${restaurant.address}</li>`);
+    });
+  } else {
+    resultsList.innerHTML = "<p class=\"text-muted text-center\">No restaurants found! Please select another category or location.</p>";
+  }
+};
 
 // Todo: select the HTML elements you need
-
+const buildSearchUrl = (category, location) => {
+  return `https://the-fork-api.students.lewagon.co/api/v1/restaurants?category=${category}&location=${location}`;
+};
 
 searchForm.addEventListener("submit", (event) => {
+  event.preventDefault();
   // Todo: Find the category selected and build the URL you will send your request to
-
+  console.log(searchForm);
+  const category = document.querySelector('input[type="radio"]:checked').value;
+  const location = document.querySelector('input[type="address"]').value;
   // Todo: Replace "the-endpoint-url" with the URL you built
-  fetch("the-endpoint-url")
+  fetch(buildSearchUrl(category, location))
     .then(response => response.json())
     .then((data) => {
+      console.log(data);
       // Todo: Insert results into the list
-
+      insertResults(data);
     });
-})
+});
