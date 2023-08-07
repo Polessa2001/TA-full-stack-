a89fdf2045f8e6aaf03f48d1a716443e154ba661
Polina Sokolova <polina2001sokolovas@gmail.com>
2023-08-07 15:41:54 +0200

    finished

--- 04-Front-End/07-AJAX/01-Name-Age/lib/index.js
+++ 04-Front-End/07-AJAX/01-Name-Age/lib/index.js
@@ -1,11 +1,16 @@
-const agifyApiUrl = "https://api.agify.io/"
-
 const displayAge = (event) => {
   event.preventDefault();
   const firstName = document.getElementById("first-name").value;
   // TODO: Display your age with an AJAX call instead of the console.log()
   console.log(firstName);
-}
+  const agifyApiUrl = `https://api.agify.io/?name=${firstName}`;
+  fetch(agifyApiUrl)
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+      document.getElementById("your-age").innerText = `You're ${data.age} years old.`; // Wait for parsing, allowing us to manipulate the data
+    });
+};
 
 const form = document.getElementById("fetch-age");
 form.addEventListener("submit", displayAge);
